97fa721df26dd0eb1fa7bb255ff8a2628eda6595
jazonandes <58129136+jazonandes@users.noreply.github.com>
2024-05-28 04:59:20 -0400

    Update FgcMode.cpp

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -1,16 +1,10 @@
 #include "modes/FgcMode.hpp"
 
 FgcMode::FgcMode(socd::SocdType horizontal_socd, socd::SocdType vertical_socd) {
-    _socd_pair_count = 4;
+    _socd_pair_count = 2;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
         socd::SocdPair{&InputState::left,   &InputState::right, horizontal_socd         },
- /* Mod X override C-Up input if both are pressed. Without this, neutral SOCD doesn't work
-  properly if Down and both Up buttons are pressed, because it first resolves Down + Mod X
-  to set both as unpressed, and then it sees C-Up as pressed but not Down, so you get an up
-  input instead of neutral. */
-        socd::SocdPair{ &InputState::mod_x, &InputState::c_up,  socd::SOCD_DIR1_PRIORITY},
         socd::SocdPair{ &InputState::down,  &InputState::mod_x, vertical_socd           },
-        socd::SocdPair{ &InputState::down,  &InputState::c_up,  vertical_socd           },
     };
 }
 
@@ -19,16 +13,19 @@ void FgcMode::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.dpadLeft = inputs.left;
     outputs.dpadRight = inputs.right;
     outputs.dpadDown = inputs.down;
-    outputs.dpadUp = inputs.mod_x || inputs.c_up;
+    outputs.dpadUp = inputs.mod_x;
 
     // Menu keys
     outputs.start = inputs.start;
-    outputs.select = inputs.c_left;
-    outputs.home = inputs.c_down;
+    outputs.select = inputs.c_right;
+    outputs.home = inputs.l && inputs.start;
+    outputs.capture = inputs.l  && inputs.mod_x;
+    outputs.leftStickClick = inputs.c_up;
+    outputs.rightStickClick = inputs.c_left;
 
     // Right hand bottom row
-    outputs.a = inputs.b;
-    outputs.b = inputs.x;
+    outputs.a = inputs.x;
+    outputs.b = inputs.b;
     outputs.triggerRDigital = inputs.z;
     outputs.triggerLDigital = inputs.up;
 
